81dff75b7b9d0029966dd68bee50f905be6b5739
KronosTheTitan <tjalle@borgersonline.nl>
2023-05-22 10:49:56 +0200

    Implemented vocal sack attack

--- Assets/Scripts/Gameplay/PlayerCombat.cs
+++ Assets/Scripts/Gameplay/PlayerCombat.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Gameplay
@@ -7,12 +8,62 @@ namespace Gameplay
     {
         [SerializeField] private Player player;
 
-        [SerializeField] private float tongueAttack;
+        private void Update()
+        {
+            if (player.GetGamepad().X.justReleased)
+            {
+                
+            }
+            
+            if (player.GetGamepad().Y.justReleased)
+            {
+                TongueHook();
+            }
+
+            if (player.GetGamepad().B.justReleased)
+            {
+                VocalSack();
+            }
+        }
 
         public void TongueHook()
         {
             RaycastHit hit;
             Physics.Raycast(transform.position + transform.forward, transform.forward, out hit);
+        }
+
+
+        [SerializeField] private int horizontalInterval = 15; 
+        [SerializeField] private int verticalInterval = 15;
+        [SerializeField] private float vocalSackRange = 1;
+        public void VocalSack()
+        {
+            for (int x = -(horizontalInterval * 2); x < horizontalInterval * 2; x += horizontalInterval)
+            {
+                Quaternion horizontal = Quaternion.AngleAxis(x , transform.up);
+                
+                for (int y = -(verticalInterval * 2); y < verticalInterval * 2; y += verticalInterval)
+                {
+                    Quaternion vertical = Quaternion.AngleAxis(y,transform.right);
+                    Quaternion direction = horizontal * vertical;
+                    
+                    RaycastHit hit;
+                    Physics.Raycast(transform.position + transform.forward / 2, direction * transform.forward, out hit, vocalSackRange);
+                    
+                    if (hit.collider == null)
+                    {
+                        Debug.Log("No collider found");
+                        continue;
+                    }
+
+                    IDamageable damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+                    if(damageable == null)
+                        continue;
+
+                    damageable.TakeDamage(transform.position - hit.collider.transform.position, player);
+                    Debug.Log("hit someone");
+                }
+            }
         }
     }
 }

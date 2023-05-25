c4dc7dfba07c603503ae4813e50b6c371e850672
KronosTheTitan <tjalle@borgersonline.nl>
2023-05-25 10:30:12 +0200

    implemented audio for the player

---
+++ Assets/Scripts/Gameplay/Player/PlayerAudio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class PlayerAudio : MonoBehaviour
+    {
+        [SerializeField] private PlayerCombat combat;
+
+        [SerializeField] private AudioSource audioSource;
+
+        [SerializeField] private AudioClip frogKickSfx;
+        [SerializeField] private AudioClip tongueHookSfx;
+        [SerializeField] private AudioClip vocalSackSfx;
+        private void Start()
+        {
+            combat.OnKick += KickAudio;
+            combat.OnTonguePull += TongueHookAudio;
+            combat.OnVocalSack += VocalSackAudio;
+        }
+
+        private void KickAudio()
+        {
+            audioSource.clip = frogKickSfx;
+            audioSource.Play();
+        }
+
+        private void TongueHookAudio()
+        {
+            audioSource.clip = vocalSackSfx;
+            audioSource.Play();
+        }
+
+        private void VocalSackAudio()
+        {
+            audioSource.clip = tongueHookSfx;
+            audioSource.Play();
+        }
+    }
+}

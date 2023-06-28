a0778788e40f28b165eb4fca77244371f366c526
Tjalle Borgers <tjalle@borgersonline.nl>
2023-06-28 15:26:42 +0200

    rollback

--- Assets/Scripts/Managers/SplitScreenManager.cs
+++ Assets/Scripts/Managers/SplitScreenManager.cs
@@ -1,95 +1,101 @@
-﻿//Class written by Tjalle Borgers on 11/05/2023
-
+﻿﻿//Class written by Tjalle Borgers on 11/05/2023
 using System;
 using System.Collections.Generic;
 using Gameplay.Player;
 using UnityEngine;
 
-namespace Managers
-{
-    /// <summary>
-    /// this struct functions as a go between to store a layout for the cameras.
-    /// I would normally do this with a 2d array but unity can't serialize that
-    /// so I would be stuck making any adjustments by hand, this way I can
-    /// move that task to the designers.
-    /// </summary>
-    [Serializable]
-    public struct CameraLayout
-    {
-        public Rect[] rects;
-    }
+ namespace Managers
+ {
+     /// <summary>
+     /// this struct functions as a go between to store a layout for the cameras.
+     /// I would normally do this with a 2d array but unity can't serialize that
+     /// so I would be stuck making any adjustments by hand, this way I can
+     /// move that task to the designers.
+     /// </summary>
+     [Serializable]
+     public struct CameraLayout
+     {
+         public Rect[] rects;
+     }
 
-    /// <summary>
-    /// This class handles the layout of the split screen cameras.
-    /// </summary>
-    public class SplitScreenManager : MonoBehaviour
-    {
-        [SerializeField] private CameraLayout[] cameraLayouts;
-        [SerializeField] private Camera[] cameras;
-        [SerializeField] private UIManager uiManager;
+     /// <summary>
+     /// This class handles the layout of the split screen cameras.
+     /// </summary>
+     public class SplitScreenManager : MonoBehaviour
+     {
+         private Dictionary<Player, Camera> _playerCameraTable = new();
 
-        private int activeCams = 0;
-        
-        public Rect GetRectForPlayerIndex(int index)
-        {
-            return cameraLayouts[activeCams].rects[index];
-        }
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="iCamera">
-        /// The new camera that will be added.
-        /// </param>
-        /// <param name="player">
-        /// This parameter is used to keep track of what camera needs to be removed when a player disconnects
-        /// </param>
-        /// <returns>
-        /// In the event that the camera limit has been reached
-        /// the function will return false.
-        /// </returns>
-        public void AddCamera(Camera iCamera, Player player)
-        {
-            cameras[GameManager.GetInstance().GetPlayerIndex(player)] = iCamera;
-            
-            UpdateCameraLayout();
-            uiManager.UpdateSplitScreen();
+         [SerializeField] private CameraLayout[] cameraLayouts;
+         [SerializeField] private List<Camera> cameras;
+         [SerializeField] private int maximumNumberOfCameras = 4;
+         [SerializeField] private UIManager uiManager;
 
-            activeCams++;
-        }
+         public Rect GetRectForPlayerIndex(int index)
+         {
+             return cameraLayouts[cameras.Count - 1].rects[index];
+         }
 
-        /// <summary>
-        /// Removes a camera and adjusts the layout accordingly.
-        /// </summary>
-        /// <param name="player">
-        /// The player whose camera needs to be removed from the layout.
-        /// </param>
-        /// <returns></returns>
-        public void RemoveCamera(Player player)
-        {
-            cameras[GameManager.GetInstance().GetPlayerIndex(player)] = null;
-            
-            UpdateCameraLayout();
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="iCamera">
+         /// The new camera that will be added.
+         /// </param>
+         /// <param name="player">
+         /// This parameter is used to keep track of what camera needs to be removed when a player disconnects
+         /// </param>
+         /// <returns>
+         /// In the event that the camera limit has been reached
+         /// the function will return false.
+         /// </returns>
+         public bool AddCamera(Camera iCamera, Player player)
+         {
+             if (cameras.Count == maximumNumberOfCameras)
+                 return false;
 
-            activeCams--;
-        }
+             cameras.Add(iCamera);
+             _playerCameraTable.Add(player, iCamera);
 
-        public int NumberOfActiveCameras()
-        {
-            return activeCams;
-        }
+             UpdateCameraLayout();
+             uiManager.UpdateSplitScreen();
 
-        /// <summary>
-        /// Updates the layout of the cameras;
-        /// </summary>
-        private void UpdateCameraLayout()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if(cameras[i] == null)
-                    continue;
-                cameras[i].rect = cameraLayouts[activeCams].rects[i];         
-            }
-        }
-    }
-}
+             return true;
+         }
+
+         /// <summary>
+         /// Removes a camera and adjusts the layout accordingly.
+         /// </summary>
+         /// <param name="player">
+         /// The player whose camera needs to be removed from the layout.
+         /// </param>
+         /// <returns></returns>
+         public bool RemoveCamera(Player player)
+         {
+             if (cameras.Count == 1)
+                 return false;
+
+             cameras.Remove(_playerCameraTable[player]);
+             _playerCameraTable.Remove(player);
+
+             UpdateCameraLayout();
+
+             return false;
+         }
+
+         public int NumberOfActiveCameras()
+         {
+             return cameras.Count;
+         }
+
+         /// <summary>
+         /// Updates the layout of the cameras;
+         /// </summary>
+         private void UpdateCameraLayout()
+         {
+             for (int i = 0; i < cameras.Count; i++)
+             {
+                 cameras[i].rect = cameraLayouts[cameras.Count - 1].rects[i];
+             }
+         }
+     }
+ }
